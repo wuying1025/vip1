1677bbe46e35836fb86210bf6edc2a4475435b37
dongxiuling <644961699@qq.com>
2020-10-17 17:23:20 +0800

    webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,3 +2,8 @@ console.log(1)
 // es6的模块化
 import './css/index.css'
 // import './scss/index.scss'
+
+const fn = () =>{
+    console.log('哈哈')
+}
+fn();
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode:'development',
     entry: {
         index: './src/index.js'
     },
@@ -17,8 +19,32 @@ module.exports = {
             },{
                 test:/\.scss$/,
                 use:['style-loader','css-loader','sass-loader']
+            },{
+                test:/\.js$/,
+                use:[
+                    {
+                        loader:'babel-loader',
+                        options:{
+                            presets:['@babel/preset-env']
+                        }
+                        // exclude:/node_modules/
+                    }
+                ]
             }
         ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/index.html',
+            hash:true
+        }),
+    ],
+    devServer:{
+        contentBase:path.resolve(__dirname,'dist'),
+        port:8080,
+        host:'localhost',
+        open:true,
+        hot:true
     }
 }
 
