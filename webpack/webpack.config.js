2923076045b2a5b8db0adf719464246e269a556a
dongxiuling <644961699@qq.com>
2020-10-17 15:29:14 +0800

    loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -13,7 +13,10 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: ['style-loader', 'css-loader','postcss-loader']
+            },{
+                test:/\.scss$/,
+                use:['style-loader','css-loader','sass-loader']
             }
         ]
     }
