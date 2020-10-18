a7b5bd344bf41eb14b9ad174e36bd690d1ea232d
dongxiuling <644961699@qq.com>
2020-10-18 15:11:13 +0800

    vuecli搭建项目

---
+++ vuecli/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    哈哈
+    <h2>dfghjk ggl; hehe</h2>
+    <test></test>
+  </div>
+</template>
+<script>
+// 1 引入组件 import  2 components声明 3使用
+import Test from "./views/Test";
+export default {
+  components: {
+    Test
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ vuecli/src/views/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h2>我是标题</h2>
+    <p>我是内容 哈哈哈</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+// scoped 作用域 当前组件生效
+.container {
+  h2 {
+    background: red;
+  }
+  p {
+    background: green;
+  }
+}
+</style>
