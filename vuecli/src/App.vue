87d3c8e765b0d7fc0467a25ee09a01cdf11b4d97
dongxiuling <644961699@qq.com>
2020-10-18 16:01:02 +0800

    router

--- vuecli/src/App.vue
+++ vuecli/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    哈哈
+    <!-- 哈哈
     <h2>dfghjk ggl; hehe</h2>
-    <test></test>
+    <test></test>-->
+    <router-view></router-view>
+
+    <router-link to="/home">首页</router-link>
+    <router-link to="/test">测试页面</router-link>
+
   </div>
 </template>
 <script>
 // 1 引入组件 import  2 components声明 3使用
-import Test from "./views/Test";
+// import Test from "./views/Test";
 export default {
-  components: {
-    Test
-  }
+  // components: {
+  //   Test
+  // }
 };
 </script>
 
