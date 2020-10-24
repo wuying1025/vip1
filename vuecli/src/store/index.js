6567ef823f059408c59c9019bff4dfd6109fa5b2
dongxiuling <644961699@qq.com>
2020-10-24 15:36:55 +0800

    vuex action

--- vuecli/src/store/index.js
+++ vuecli/src/store/index.js
@@ -1,20 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   // 存储状态
   state: {
-    count:0
+    count: 0,
+    count2: 1,
+    list: []
   },
   mutations: {
     // count增加方法 第一个参数是state 第二个参数是传进来的参数
-    add(state,num){
+    add(state, num) {
       state.count += num;
+    },
+    // 修改state中list
+    addList(state, arr) {
+      state.list = arr;
+    }
+  },
+  // 相当于计算属性
+  getters: {
+    // sum 返回count 和coun2计算的求和
+    sum(state) {
+      return state.count + state.count2;
     }
   },
   actions: {
+    /* 调用豆瓣接口文档
+     `https://frodo.douban.com/api/v2/subject_collection/movie_showing/items?start=${event.start}&count=${event.count}&apiKey=054022eaeae0b00e0fc068c0c0a2102a`
+      start和count是分页的参数
+    */
+    // 1封装一个请求action
+    getMovieList({ commit }) {
+      console.log('调用了')
+      // 1请求电影数据
+      axios.get('https://bird.ioliu.cn/v1?url=https://frodo.douban.com/api/v2/subject_collection/movie_showing/items?start=0&count=10&apiKey=054022eaeae0b00e0fc068c0c0a2102a')
+        .then((res) => {
+          console.log(res.data.subject_collection_items);
+          // 2请求回来数据 -> 存在list中（调用mutation中的addList方法）
+          commit('addList', res.data.subject_collection_items)
+        }).catch((error) => {
+          console.log(error);
+        })
+    }
+
+
+    // 数据回来修改list ->调用mutations
+
   },
   modules: {
   }
--- vuecli/src/views/Vuex.vue
+++ vuecli/src/views/Vuex.vue
@@ -9,23 +9,34 @@
     {{count}}
     <!-- 
         $store.commit 触发mutations中的方法
-     -->
+    -->
     <button @click="$store.commit('add',5)">++</button>
     <button @click="add(6)">++</button>
-
+    <h2>count的值为{{count}}</h2>
+    <h2>count2的值为{{$store.state.count2}}</h2>
+    <h2>
+      sum的值为 ： {{$store.getters.sum}}
+    </h2>
+    <h2>sum的值为{{sum}}</h2>
   </div>
 </template>
 
 <script>
-import { mapState ,mapMutations} from "vuex";
-
+import { mapState, mapMutations ,mapGetters} from "vuex";
+import axios from 'axios'
 export default {
-    computed:{
-        ...mapState(['count'])
-    },
-    methods:{
-        ...mapMutations(['add'])
-    }
+  computed: {
+    ...mapState(["count"]),
+    ...mapGetters(['sum'])
+  },
+  methods: {
+    ...mapMutations(["add"])
+  },
+
+  created(){
+     // action中的方法 dispatch调用
+      this.$store.dispatch('getMovieList');
+  }
 };
 </script>
 
