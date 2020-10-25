d8cc19be8ca0c5e2870ab0553be46826cad788d0
dongxiuling <644961699@qq.com>
2020-10-25 10:19:12 +0800

    仿美团

---
+++ meituan/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Index from '@/views/home/index'
+import Detail from '@/views/detail/index'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path:'/',
+    component:Index
+  },{
+    path:'/detail',
+    component:Detail
+  }
+  
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
