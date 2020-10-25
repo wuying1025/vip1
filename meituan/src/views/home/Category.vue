4d8d32f41fa5acdd7f313ae2b6bb865bdcf85370
dongxiuling <644961699@qq.com>
2020-10-25 11:53:13 +0800

    首页

--- meituan/src/views/home/Category.vue
+++ meituan/src/views/home/Category.vue
@@ -1,12 +1,35 @@
 <template>
     <div>
-中间分类
+        <ul>
+            <li v-for="obj in cateList" :key="obj.id">
+                <img :src="obj.img" alt="">
+                <p>{{obj.name}}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        
+        data(){
+            return {
+                cateList:[]
+            }
+        },
+        created(){
+            axios.get('http://admin.gxxmglzx.com/tender/test/get_type')
+            .then((res)=>{ //成功
+                // axios返回的数据 res.data
+                console.log(res.data)
+                let result = res.data;
+                if(result.errcode == 200){
+                    this.cateList = result.data;
+                }
+            }).catch((error)=>{ //失败
+                console.log(error)
+            })
+        }
     }
 </script>
 
