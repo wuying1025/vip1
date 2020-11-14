849503a1948d4f5c465d8fd4b7e9d17c06c58ccf
dongxiuling <644961699@qq.com>
2020-11-14 09:48:55 +0800

    设置左右betterScroll

--- meituan/src/views/order/index.vue
+++ meituan/src/views/order/index.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="order-container">
     <!-- 左侧分类 -->
-    <ul class="cate-box">
-      <li v-for="obj in nav" :key="obj.id">{{obj.name}}</li>
-    </ul>
+    <div class="cate-box">
+      <ul>
+        <li v-for="obj in nav" :key="obj.id">{{obj.name}}</li>
+      </ul>
+    </div>
+
     <!-- 右侧商品 -->
     <div class="pro-box">
-      <div v-for="(obj,index) in goods" :key="index">
-        <h2>{{obj.name}}</h2>
-        <ul>
-          <li class="prod-list" v-for="prod in obj.content" :key="prod.id">
-            <img class="prod-img" :src="prod.img" alt />
-            <p>{{prod.name}}</p>
-          </li>
-        </ul>
+      <div>
+        <div v-for="(obj,index) in goods" :key="index">
+          <h2>{{obj.name}}</h2>
+          <ul>
+            <li class="prod-list" v-for="prod in obj.content" :key="prod.id">
+              <img class="prod-img" :src="prod.img" alt />
+              <p>{{prod.name}}</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -21,6 +26,7 @@
 
 <script>
 import axios from "axios";
+import BetterScroll from "better-scroll";
 export default {
   data() {
     return {
@@ -37,6 +43,18 @@ export default {
         console.log(res.data.data);
         this.nav = res.data.data.nav;
         this.goods = res.data.data.goods;
+
+        // 渲染到页面后 -> new BetterScroll
+        this.$nextTick(() => {
+          let cateScroll = new BetterScroll(".cate-box", {
+            click: true,
+            bounce: false
+          });
+          let prdScroll = new BetterScroll(".pro-box", {
+            click: true,
+            bounce: false
+          });
+        });
       });
   }
 };
@@ -46,6 +64,8 @@ export default {
 .order-container {
   display: flex;
   .cate-box {
+    // 视口高度-上44 -下50
+    height: calc(100vh - 94px);
     flex: 1;
     li {
       padding: 0.18rem 0.24rem 0.44rem;
@@ -54,13 +74,14 @@ export default {
   }
   .pro-box {
     flex: 3;
+    height: calc(100vh - 94px);
     .prod-list {
-        display: flex;
+      display: flex;
       .prod-img {
         width: 1.5rem;
       }
-      p{
-          flex:1
+      p {
+        flex: 1;
       }
     }
   }
